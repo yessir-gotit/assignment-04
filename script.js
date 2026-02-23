79ea25e09cfa9b3c8a8f39d51ad9bcbd1327baf0
yessir-gotit <218499181+yessir-gotit@users.noreply.github.com>
2026-02-23 22:20:34 +0600

    optimized & rewrote the entire codebase

--- script.js
+++ script.js
@@ -1,138 +1,153 @@
-let interviewNum = [];
-let rejectNum = [];
+const tabContainer = document.querySelector('.tab-btn');
+const cardContainer = document.getElementById('allCard');
 
-let all = document.getElementById('total');
-let interviewCount = document.getElementById('interview');
-let rejectCount = document.getElementById('rejected');
-let allCard = document.getElementById('allCard');
-
-const allBtn = document.getElementById('all-filter-btn');
-const interviewBtn = document.getElementById('interview-filter-btn');
-const rejectBtn = document.getElementById('reject-filter-btn');
-const emptyState = document.getElementById('empty-state');
-const jobs = document.getElementById('up-menu');
-
-function cardCount() {
-    all.innerText = allCard.querySelectorAll('.card').length;
-    interviewCount.innerText = interviewNum.length;
-    rejectCount.innerText = rejectNum.length;
-}
-
-cardCount();
-
-function toggleStyle(id) {
-    const buttons = [allBtn, interviewBtn, rejectBtn];
-    for (const btn of buttons) {
-        btn.classList.add('border-gray-200', 'text-gray-500');
-        btn.classList.remove('bg-blue-500', 'text-white');
-    }
-
-    const selected = document.getElementById(id);
-    selected.classList.remove('border-gray-200', 'text-gray-500');
-
-    selected.classList.add('bg-blue-500', 'text-white');
-
-    const cards = allCard.querySelectorAll('.card');
-    let count = 0;
-
-    for (const card of cards) {
-        card.classList.add('shadow-md');
-        const badgeText = card.querySelector('.badge-text').innerText;
-
-        if (id === 'all-filter-btn') {
-            card.style.display = 'block';
-            count++;
-        } 
-        else if (id === 'interview-filter-btn') {
-            if (badgeText === 'INTERVIEWED') {
-                card.style.display = 'block';
-                count++;
-            } 
-            
-            else {
-                card.style.display = 'none';
-            }
-        } 
-        else if (id === 'reject-filter-btn') {
-            if (badgeText === 'REJECTED') {
-                card.style.display = 'block';
-                count++;
-            } 
-            
-            else {
-                card.style.display = 'none';
-            }
-        }
-    }
-
-    if (count === 0) {
-        emptyState.classList.remove('hidden');
-        allCard.classList.add('hidden'); 
-    } 
+tabContainer.addEventListener('click', function(e) {
+    const target = e.target;
     
-    else {
-        emptyState.classList.add('hidden');
-        allCard.classList.remove('hidden');
-    }
-
-    jobs.innerText = `${count} jobs`;
-}
-
-allCard.addEventListener('click', function(e) {
-    const t = e.target;
-    const trash = t.closest('.btn-ghost'); 
-
-    const isInt = t.classList.contains('interview-btn');
-    const isRej = t.classList.contains('reject-btn');
-
-    if (trash && !isInt && !isRej) {
-
-        const p = trash.closest('.card');
-
-        const name = p.querySelector('.company-name').innerText;
-
-        interviewNum = interviewNum.filter(i => i !== name);
-        rejectNum = rejectNum.filter(i => i !== name);
-        
-        p.remove();
-        cardCount();
-        
-        const cur = [allBtn, interviewBtn, rejectBtn].find(b => b.classList.contains('bg-blue-500'));
-        toggleStyle(cur.id);
+    if (!target.classList.contains('btn')) {
         return;
     }
 
-    if (!isInt && !isRej) return;
-
-    const p = t.closest('.card');
-    const name = p.querySelector('.company-name').innerText;
-    const b = p.querySelector('.badge-text');
-
-    if (isInt) {
-        b.innerText = "INTERVIEWED";
-        b.classList.remove('bg-blue-50', 'text-[#0D2A54]', 'bg-red-50', 'text-red-600');
-        b.classList.add('bg-green-100', 'text-green-700');
-
-        if (!interviewNum.includes(name)) {
-            interviewNum.push(name);
-            rejectNum = rejectNum.filter(i => i !== name);
-        }
-    } 
-
-    else if (isRej) {
-        b.innerText = "REJECTED";
-        b.classList.remove('bg-blue-50', 'text-[#0D2A54]', 'bg-green-100', 'text-green-700');
-        b.classList.add('bg-red-50', 'text-red-600');
-
-        if (!rejectNum.includes(name)) {
-            rejectNum.push(name);
-            interviewNum = interviewNum.filter(i => i !== name);
-        }
+    const active = tabContainer.querySelector('.active');
+    if (active){
+        active.classList.remove('active');
     }
 
-    cardCount(); 
-    const act = [allBtn, interviewBtn, rejectBtn].find(btn => btn.classList.contains('bg-blue-500')).id;
-    toggleStyle(act);
-
+    target.classList.add('active');
+    filterCards();
+    updateMenu();
 
 })
+
+function updateStats(){
+    let allCount = Number(0);
+    let intCount = Number(0);
+    let rejCount = Number(0);
+    for (const card of cardContainer.children){
+        if (card.getAttribute('data-status') === 'interview') {
+            intCount++;
+        }
+
+        else if (card.getAttribute('data-status') === 'reject') {
+            rejCount++;
+        }
+
+    
+    
+    }
+    allCount = cardContainer.children.length;
+
+    document.getElementById('interview').innerText = intCount;
+    document.getElementById('rejected').innerText = rejCount;
+    document.getElementById('total').innerText = allCount;
+}
+
+function updateMenu() {
+    const menu = document.getElementById('up-menu');
+    let count = 0;
+    const unga = cardContainer.children;
+    for (const c of unga) {
+        if (c.style.display !== 'none') {
+            count++;
+        }
+    }
+    
+    menu.innerText = `${count} jobs`;
+
+}
+
+
+function filterCards(){
+    const active = document.querySelector('.tab-btn .active');
+    const card = cardContainer.children;
+    const empty = document.getElementById('empty-state');
+    let cardsShownInt = 0;
+    let cardsShownRej = 0;
+    let allShown = 0;
+    for (const c of card) {
+        
+
+        if (active.id === 'interview-filter-btn') {
+
+            if (c.getAttribute('data-status') === 'interview') {
+                c.style.display = 'block';
+                cardsShownInt++;
+            }
+            else {
+                c.style.display = 'none';
+            }
+        }
+
+        else if(active.id === 'reject-filter-btn'){
+            if (c.getAttribute('data-status') === 'reject') {
+                c.style.display = 'block';
+                cardsShownRej++;
+            }
+            else {
+                c.style.display = 'none';
+            }
+        }
+        else{
+            c.style.display = 'block';
+            allShown++;
+        }
+    }
+    
+    if (cardsShownInt === 0 && active.id === 'interview-filter-btn'){
+        empty.classList.remove('hidden');
+    }
+    else if (cardsShownRej === 0 && active.id === 'reject-filter-btn'){
+        empty.classList.remove('hidden');
+    }
+    else if (active.id === 'all-filter-btn' && allShown === 0) {
+        empty.classList.remove('hidden');
+    }
+
+    else{
+        empty.classList.add('hidden');
+    }
+
+    
+}
+
+
+cardContainer.addEventListener('click', function(e) {
+    const target = e.target;
+    const btn = target.closest('.btn');
+
+    if (!btn) {
+        return;
+    }
+    const card = btn.closest('.card');
+    const badge = card.querySelector('.badge');
+
+    if(btn.classList.contains('delete')){
+        card.remove();
+        updateStats();
+        filterCards();
+        updateMenu();
+        return;
+    }
+        
+    if (btn.classList.contains('interview-btn')){
+        target.closest('.card').setAttribute('data-status', 'interview');
+        badge.innerText = "INTERVIEWED";
+        badge.classList.remove('bg-blue-50', 'text-[#0D2A54]', 'bg-red-50', 'text-red-600');
+        badge.classList.add('bg-green-100', 'text-green-700');
+    }
+
+    else if (btn.classList.contains('reject-btn')){
+        target.closest('.card').setAttribute('data-status', 'reject');
+        badge.innerText = "REJECTED";
+        badge.classList.remove('bg-blue-50', 'text-[#0D2A54]', 'bg-green-100', 'text-green-700');
+        badge.classList.add('bg-red-50', 'text-red-600');
+    }
+
+    updateStats();
+    filterCards();
+    updateMenu();
+})
+updateStats();
+filterCards();
+updateMenu();
+
